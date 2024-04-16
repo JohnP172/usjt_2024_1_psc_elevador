54a6b2b9d7b1ef45b61832a87495b1434a0c6b5f
João Pedro Correia <jpedrocorreia172@outlook.com>
2024-04-15 21:39:21 -0300

    Versão Inicial.

---
+++ src/main/java/br/usjt/elevador/TelaElevador.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.usjt.elevador;
+
+/**
+ *
+ * @author João Pedro da Silva Correia
+ */
+public class TelaElevador {
+    
+}
